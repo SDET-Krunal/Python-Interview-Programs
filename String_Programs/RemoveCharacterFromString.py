fe6da53d925c141b7c2fba22cb4c80e848307477
Krunal Panchal <Krunal_Panchal@musikaar.com>
2022-01-29 11:21:54 +0530

    Added char removal program

---
+++ String_Programs/RemoveCharacterFromString.py
@@ -0,0 +1,13 @@
+def remove_char(s: str, c: str):
+    ns = ""
+
+    for i in s:
+        if c != i:
+            ns += i
+
+    return ns
+
+
+print("Given string after removing given character ::", remove_char("krunal", 'r'))
+
+# Output :: kunal
